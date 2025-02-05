c5837493a9c73e9cc49a451575aa6097e96aa1a2
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2025-02-05 14:01:38 +0200

    add video player and playlist video

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import App from "./app/App";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+/*     "video.js": "7.12.3",
+    "videojs-contrib-quality-levels": "2.1.0",
+    "videojs-hls-quality-selector": "1.1.4", */
